0dcd4e638b9a431684c46c8bdbb248dd2f5203da
jibran <jibranshawl@gmail.com>
2018-03-25 23:44:59 +0530

    changed

---
+++ nav.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+
+	 $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+
+
+})// end doc ready
